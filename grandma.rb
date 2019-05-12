77149dac092427f8dc28d7e7a9591165a4b6a415
Alex Siler <davidalexsiler@gmail.com>
2019-05-12 06:28:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,10 @@
 
 # Write a speak_to_grandma method.
 def speak_to_grandma(str)
-  if str == str.upcase
+  if str == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif str == str.upcase
     puts "NO, NOT SINCE 1938!"
-  elsif str == "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
   else 
     puts "HUH?! SPEAK UP SONNY"
   end
