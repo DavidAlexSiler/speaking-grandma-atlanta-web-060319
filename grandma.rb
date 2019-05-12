728877ca013c6d9e3262fbd3b52752e758e76eeb
Alex Siler <davidalexsiler@gmail.com>
2019-05-12 06:26:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,8 +7,8 @@ def speak_to_grandma(str)
     puts "I LOVE YOU TOO PUMPKIN!"
   else 
     puts "HUH?! SPEAK UP SONNY"
-end
-  
+  end
+end 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
